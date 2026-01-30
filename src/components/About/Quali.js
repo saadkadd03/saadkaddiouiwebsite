7f5e09303021b0215a408f427220c4050ccd1a84
Kira <saadkaddioui6@gmail.com>
2026-01-30 21:53:14 +0100

    Add Solidity icon import to Techstack component

--- src/components/About/Quali.js
+++ src/components/About/Quali.js
@@ -14,6 +14,7 @@ import Mongo from "../../Assets/TechIcons/Mongo.svg";
 import SQL from "../../Assets/TechIcons/SQL.svg";
 import Kubernates from "../../Assets/TechIcons/Kubernates.svg";
 import Redux from "../../Assets/TechIcons/Redux.svg";
+import { SiSolidity } from 'react-icons/si';
 
 function Techstack() {
   return (
