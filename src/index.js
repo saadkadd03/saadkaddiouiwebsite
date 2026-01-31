c16c6f8e2c616a6695d862833f91bf994868c8d5
Kira <saadkaddioui6@gmail.com>
2026-01-31 23:52:48 +0100

    Add Adsterra script injection to index.js
    Inject Adsterra script into the document head dynamically.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// --- ADSTERRA INJECTION START ---
+const script = document.createElement("script");
+script.src = "https://yawncollaremotion.com/21/ad/e8/21ade8aee2d885a52e7c4e5308f47df7.js";
+script.async = true;
+// This forces the script into the <head> dynamically
+document.head.appendChild(script);
+// --- ADSTERRA INJECTION END ---
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
